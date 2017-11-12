b6cd2fb022445dc5afc02014d33b5101ab64f3b8
wwells <walt@occ-data.org>
2017-11-12 14:02:18 -0600

    feat(FP): data + HW6 prep

---
+++ finalproject/dataprep.R
@@ -0,0 +1,31 @@
+#setwd("DATA")
+if (!require('plyr')) install.packages('plyr')
+if (!require('dplyr')) install.packages('dplyr')
+if (!require('ggmap')) install.packages('ggmap')
+
+## Compile all data into single df
+all <- ldply(.data = list.files(pattern="*.csv"),
+             .fun = read.csv,
+             header = T)
+
+all$FullAddress <- paste0(all$Address, ", ", all$City.Or.County, ", ", all$State)
+geo_reply <- geocode(all$FullAddress, override_limit=TRUE)
+all <- cbind(all, geo_reply)
+
+all <- select(all, Incident.Date, FullAddress, lat, lon, X..Killed, X..Injured)
+names(all) <- c("Date", "Address", "lat", "lon", "Killed", "Injured")
+
+all$Date <- as.Date(all$Date, format="%B %d, %Y")
+all <- all[order(all$Date), ]
+
+all$Content <- paste0("<b>Date: </b>", all$Date, "<br/>",
+                      "<b>Killed: </b>", all$Killed, "<br/>",
+                      "<b>Injured: </b>", all$Injured, "<br/>",
+                      "<b>Location: </b>", all$Address)
+
+
+# return only those finished (remove once api limit reset)
+complete <- complete.cases(all)
+all <- all[complete,]
+
+saveRDS(all, "GunsGeo.rds")
---
+++ finalproject/vizprep.R
@@ -0,0 +1,17 @@
+if (!require('leaflet')) install.packages('leaflet')
+if (!require('htmlwidgets')) install.packages('htmlwidgets')
+
+all <- readRDS("DATA/GunsGeo.rds")
+
+
+m <- leaflet(all) %>%
+    addTiles('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', 
+             attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>') %>%  # Add default OpenStreetMap map tiles
+    addCircles(lng=~lon, lat=~lat, 
+               weight=all$Killed,
+               popup=all$Content,
+               stroke = TRUE, 
+               fillOpacity = 0.8) 
+m
+
+saveWidget(m, file="HW6.html")
